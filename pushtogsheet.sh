cabc2773f64e5d21b2a65f659d8662c3e34432fb
JoshKing56 <joshuaiking56@gmail.com>
2018-01-30 13:45:15 +0000

    Got the curl command to work

--- pushtogsheet.sh
+++ pushtogsheet.sh
@@ -3,25 +3,25 @@
 #TODO: https://archive.is/TRzn4 Rewrite structure with getops
 
 #Set default values
-FORMID=1N42kppOkVqbX5Ca4zluYjEJyKZNjZP9lPKfgTuzVzzU;
+FORMID="1gcF_E81EpBX6DaSV8pBwKeEY6XCBxKLslwtIKwI8WAE";
 FIELDNUM=1;
-FIELDNAME=2097356952;
+FIELDS[0]=713884440;
 
 echo ""
 echo "Enter google form credentials"
-read -p "Form Id (in url): " FORMID
-read -p  "Enter number of fields: " FIELDNUM
+#read -p "Form Id (in url): " FORMID
+#read -p  "Enter number of fields: " FIELDNUM
 
-echo "Enter the field names and values. This is your html \"name\" attribute"
+#echo "Enter the field names and values. This is your html \"name\" attribute"
 
-  for ((i=0;i<$FIELDNUM;i++)) #for some reason bash requires the (()) for for loops idk why
-  do
-    read -p "Field name $i: " FIELDS[$i]
-    read -p "Field value $i: " VALUES[$i]
-  done;
+for ((i=0;i<$FIELDNUM;i++)) #for some reason bash requires the (()) for for loops idk why
+do
+#  read -p "Field name $i: " FIELDS[$i]
+  read -p "Field value $i: " VALUES[$i]
+done;
 
-  for ((i=0;i<${#FIELDS[*]};i++))
-  do
-    echo "Done $i out of ${#FIELDS[*]}"
-    curl https://docs.google.com/forms/d/$FORMID/formResponse -d ifq -d entry.$FIELDS[$i]=$VALUES[$i] submit=Submit
+for ((i=0;i<${#FIELDS[*]};i++))
+do
+  echo "Done $i out of ${#FIELDS[*]}";
+  curl https://docs.google.com/forms/d/$FORMID/formResponse -d ifq -d entry.${FIELDS[$i]}="${VALUES[$i]}" -d submit=Submit > /dev/null;
 done;
