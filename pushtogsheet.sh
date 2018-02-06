8fee8ce717c67146fee9e13583e45c6169f65def
JoshKing56 <joshuaiking56@gmail.com>
2018-02-06 12:21:38 +0000

    Figure out how to surpress curl output, it's -s

--- pushtogsheet.sh
+++ pushtogsheet.sh
@@ -2,7 +2,7 @@
 
 #TODO: https://archive.is/TRzn4 Rewrite structure with getops
 
-#Set default values
+#Read from conf file
 FORMID=$(sed -n 's/FORMID: //p' formdata.conf); #like grep, but with different syntax.
 FIELDNUM=$(sed -n 's/FIELDNUM: //p' formdata.conf); #looks for everything matching "Everything after FIELDNUM:"
 FIELDS[0]=$(sed -n 's/FIELDS: //p' formdata.conf);
@@ -13,9 +13,8 @@ do
   read -p "Field value $i: " VALUES[$i]
 done;
 
-
 for ((i=0;i<${#FIELDS[*]};i++))
 do
   echo "Done $i out of ${#FIELDS[*]}";
-  curl https://docs.google.com/forms/d/$FORMID/formResponse -d ifq -d entry.${FIELDS[$i]}="${VALUES[$i]}" -d submit=Submit > /dev/null;
+  curl -s https://docs.google.com/forms/d/$FORMID/formResponse -d ifq -d entry.${FIELDS[$i]}="${VALUES[$i]}" -d submit=Submit > /dev/null;
 done;
