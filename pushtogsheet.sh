196822859378a2859901bdd479ef809e3afe0b0d
JoshKing56 <joshuaiking56@gmail.com>
2018-02-06 20:35:17 +0000

    IT'S DONE!!!

--- pushtogsheet.sh
+++ pushtogsheet.sh
@@ -2,24 +2,28 @@
 
 #TODO: https://archive.is/TRzn4 Rewrite structure with getops
 
+CONFFILE="formdata.conf"
+if [ -n $1 ]; then #-n says "if string is not zero"
+    CONFFILE=$1;
+fi
+
 #Read from conf file
 FORMID=$(sed -n 's/FORMID: //p' formdata.conf); #like grep, but with different syntax.
 FIELDNUM=$(sed -n 's/FIELDNUM: //p' formdata.conf); #looks for everything matching "Everything after FIELDNUM:"
 FIELDS[0]=$(sed -n 's/FIELDS: //p' formdata.conf);
 
-#for ((i=0;i<$FIELDNUM;i++)) #for some reason bash requires the (()) for for loops idk why
-#do
-  #FIELDS[0]=$(sed -n 's/FIELDS[${i}]: //p' formdata.conf); #uses the sed command to get values
-#  read -p "Field value $i: " VALUES[$i] # just asks the value
-#done;
-
-VALUES=$1;
-
-
-
-
-for ((i=0;i<${#FIELDS[*]};i++))
+for ((i=0;i<$FIELDNUM;i++)) #for some reason bash requires the (()) for for loops idk why
 do
-  echo "Done $i out of ${#FIELDS[*]}";
-  curl -s https://docs.google.com/forms/d/$FORMID/formResponse -d ifq -d entry.${FIELDS[$i]}="${VALUES}" -d submit=Submit > /dev/null;
+    FIELDS[$i]=$(sed -n "s/FIELDS\[$i\]: //p" formdata.conf); #uses the sed command to get values
+    VALUES[$i]=$(sed -n "s/VALUE\[$i\]: //p" formdata.conf);
 done;
+
+
+ENTRYSTRING="";
+for ((i=0;i<$FIELDNUM;i++))
+do
+    ENTRYSTRING+=" -d entry.${FIELDS[$i]}=${VALUES[$i]}";
+done;
+
+curl -s https://docs.google.com/forms/d/$FORMID/formResponse -d ifq$ENTRYSTRING -d submit=Submit > /dev/null;
+echo "Done $i out of ${#FIELDS[*]}";
--- speedtest-cli.sh
+++ speedtest-cli.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-#while /bin/true; do
+while /bin/true; do
+    cp formtemplate.conf formdata.conf;
+    echo "" >> formdata.conf;
+
     RAWSTRING=$(speedtest-cli --csv);
+    FIELDS=(701604493 40350831 761314166 1882583532 1039993291 1098080092 958097284 2132018028)
+    PUSHSTRING=""
     for i in {1..8}
     do
         CUTSTRING=$(echo $RAWSTRING | cut -d , -f $i);
-        echo $CUTSTRING;
-        bash pushtogsheet.sh $CUTSTRING
+        PUSHSTRING="FIELDS[$(expr $i - 1)]: ${FIELDS[$(expr $i - 1)]}\nVALUE[$(expr $i - 1)]: $CUTSTRING";
+        echo -e $PUSHSTRING >> formdata.conf
     done;
+
+    bash pushtogsheet.sh
+
+    sleep 10m;
+
+done;
